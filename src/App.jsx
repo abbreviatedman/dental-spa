9d5bde0f68872f78809cfc9b26b14f3ec4eea17c
Colin Jaffe <balloonasaurus@gmail.com>
2021-03-09 17:00:15 -0500

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Dental Spa</h1>
+      <p>Welcome to the home of The Tooth Soothers!</p>
+    </Fragment>
+  );
+}
+
+export default App;
